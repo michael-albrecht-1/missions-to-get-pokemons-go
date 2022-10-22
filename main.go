5501124513cc2e51cde7ac74f1c541939c7daec9
michael.albrecht <albrecht.michael@hotmail.com>
2022-10-22 23:39:55 +0200

    /missions

--- main.go
+++ main.go
@@ -8,26 +8,33 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
+	router.GET("/missions", getMissions)
 
 	router.Run("localhost:8080")
 }
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+type Status string
+const (
+    Created Status = "created"
+    Done Status = "done"
+    Expired Status =  "expired"
+)
+
+type Mission struct {
+	Uuid         string `json:"uuid"`
+	Title        string `json:"title"`
+	Description  string `json:"description"`
+	Rewards      string `json:"rewards"`
+	Status       Status `json:"status"`
+	DateCreation string `json:"dateCreation"`
 }
 
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+
+var missions = []Mission{
+	{Uuid: "1", Title: "lave la vaisselle feignasse", Description: "description", Rewards: "rewards", Status: Done, DateCreation: "2022/10/22"},
+	{Uuid: "2", Title: "lave la vaisselle feignasse 2", Description: "description", Rewards: "rewards", Status: "status", DateCreation: "2022/10/22"},
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+func getMissions(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, missions)
 }
